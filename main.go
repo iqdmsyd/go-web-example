f5ea067b7501f31c119b1350ac3916f1812008bd
Iqdam Musayyad Rabbani <iamusayyad@gmail.com>
2023-04-26 21:42:43 +0700

    add routing (gorilla/mux)

--- main.go
+++ main.go
@@ -3,18 +3,72 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	// process dynamic requests
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprint(w, "Hello Dek")
+	// })
+
+	// server static assets
+	// fs := http.FileServer(http.Dir("static/"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// accept connections
+	// http.ListenAndServe(":8080", nil)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello Dek")
 	})
 
-	// server static assets
 	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 
-	// accept connections
-	http.ListenAndServe(":8080", nil)
+	router.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page: %s\n", title, page)
+	})
+
+	router.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	router.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	router.HandleFunc("/books/{id}", UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", DeleteBook).Methods("DELETE")
+
+	http.ListenAndServe(":8080", router)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	fmt.Fprintf(w, "POST /books/%s\n", title)
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	fmt.Fprintf(w, "GET /books/%s\n", title)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	fmt.Fprintf(w, "PUT /books/%s\n", id)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	fmt.Fprintf(w, "DEL /books/%s\n", id)
 }
