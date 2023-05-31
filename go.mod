a7d89176327fac7b5fbbac1870bf0e88bcb50b10
Iqdam Musayyad Rabbani <iamusayyad@gmail.com>
2023-05-31 20:29:54 +0700

    add postgres database

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/iqdmsyd/go-web-example
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -42,6 +46,11 @@ func main() {
 	router.HandleFunc("/books/{id}", UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", DeleteBook).Methods("DELETE")
 
+	router.HandleFunc("/records", GetAllRecord).Methods("GET")
+	router.HandleFunc("/records/{id}", GetRecordById).Methods("GET")
+	router.HandleFunc("/records/{band}/{song}", CreateRecord).Methods("POST")
+	router.HandleFunc("/records/{id}", DeleteRecord).Methods("DELETE")
+
 	http.ListenAndServe(":8080", router)
 }
 
@@ -71,4 +80,138 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	id := vars["id"]
 
 	fmt.Fprintf(w, "DEL /books/%s\n", id)
+}
+
+type record struct {
+	Id   int32
+	Band string
+	Song string
+}
+
+func GetAllRecord(w http.ResponseWriter, r *http.Request) {
+	// connection string
+	connStr := "postgres://mydb:mydb@localhost"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// query data
+	rows, err := db.Query("SELECT id, band, song FROM records")
+	if err != nil {
+		errRes := map[string]string{"error": "Data not found"}
+		errJSON, _ := json.Marshal(errRes)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(errJSON)
+		return
+	}
+	defer rows.Close()
+
+	// scan query result
+	var result []record
+	for rows.Next() {
+		var r record
+		err := rows.Scan(&r.Id, &r.Band, &r.Song)
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, r)
+	}
+
+	// encode result to json
+	resJSON, _ := json.Marshal(result)
+
+	// set response header
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resJSON)
+}
+
+func GetRecordById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	connStr := "postgres://mydb:mydb@localhost"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result record
+	row := db.QueryRow("SELECT id, band, song FROM records WHERE id = $1", id).Scan(&result.Id, &result.Band, &result.Song)
+
+	if row != nil {
+		errRes := map[string]string{"error": "Data not found"}
+		errJSON, _ := json.Marshal(errRes)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(errJSON)
+		return
+	}
+
+	resJSON, _ := json.Marshal(result)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resJSON)
+}
+
+func CreateRecord(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	band := vars["band"]
+	song := vars["song"]
+
+	// connection string
+	connStr := "postgres://mydb:mydb@localhost"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var id int32
+	row := db.QueryRow(`INSERT INTO records(band, song)
+		VALUES($1, $2) RETURNING id`, band, song).Scan(&id)
+	if row != nil {
+		log.Fatal(row)
+	}
+
+	result := record{Id: id, Band: band, Song: song}
+	resJSON, _ := json.Marshal(result)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(resJSON)
+}
+
+func DeleteRecord(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	connStr := "postgres://mydb:mydb@localhost"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var rec record
+	row := db.QueryRow("SELECT id, band, song FROM records WHERE id = $1", id).Scan(&rec.Id, &rec.Band, &rec.Song)
+	if row != nil {
+		errRes := map[string]string{"error": "Data not found"}
+		errJSON, _ := json.Marshal(errRes)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(errJSON)
+		return
+	}
+
+	db.QueryRow("DELETE FROM records WHERE id = $1", id)
+	response := map[string]string{"data": ""}
+	resJSON, _ := json.Marshal(response)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resJSON)
 }
